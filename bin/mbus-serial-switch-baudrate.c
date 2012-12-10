dc948566db78ad9295e3fc68cfc01e5395f796fc
Stefan Wahren <info@lategoodbye.de>
2012-12-10 21:26:48 +0100

    remove unnecessary POSIX header

--- bin/mbus-serial-switch-baudrate.c
+++ bin/mbus-serial-switch-baudrate.c
@@ -11,10 +11,6 @@
 //
 //------------------------------------------------------------------------------
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
 #include <string.h>
 
 #include <stdio.h>
