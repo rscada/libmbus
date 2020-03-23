491562be8b6e94cbfd6716c089e1e23c5313aa0d
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-03-23 09:38:47 +0100

    feat: add github actions
    * feat: add github actions
    
    * Update ccpp.yml
    
    * build: build and install deb in container
    
    * build: clean up

--- build.sh
+++ build.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+
 rm -rf _build
 mkdir _build
 cd _build
 cmake .. -DLIBMBUS_BUILD_EXAMPLES=ON -DLIBMBUS_BUILD_TESTS=ON
 cmake --build . -j
+
+# build deb
+
+# rm -rf _build
+# mkdir _build
+# cd _build
+# cmake .. -DLIBMBUS_PACKAGE_DEB=ON
+# cpack ..
+# dpkg -i *.deb
