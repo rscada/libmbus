d7eacdc38f33391161d1bd2e28906948d7547a0d
Carlos Gomes Martinho <carlos.gomes_martinho@siemens.com>
2020-04-24 09:55:01 +0200

    chore: make path to tests optional
    (cherry picked from commit f569816788887b2052761d1574d6662b75238172)

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 
-rm -rf _build
-mkdir _build
-cd _build
+rm -rf build
+mkdir build
+cd build
 cmake .. -DLIBMBUS_BUILD_EXAMPLES=ON -DLIBMBUS_BUILD_TESTS=ON
 cmake --build .
 
 # build deb
 
-# rm -rf _build
-# mkdir _build
-# cd _build
+# rm -rf build
+# mkdir build
+# cd build
 # cmake .. -DLIBMBUS_PACKAGE_DEB=ON
 # cpack ..
 # dpkg -i *.deb
