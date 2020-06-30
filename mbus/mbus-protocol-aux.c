7297899a89d727fcf1894f4e0253f99d94bca9cb
Fredrik Skold <fredrik.skold@hiq.se>
2020-06-30 13:33:45 +0200

    Make mbus_parse_variable_record forward error #169
    Update mbus_parse_variable_record so it return NULL if
    mbus_parse_variable_record return NULL.
    This makes the program mbus_parse_hex exit with non zero exitcode.

--- mbus/mbus-protocol-aux.c
+++ mbus/mbus-protocol-aux.c
@@ -1455,6 +1455,8 @@ mbus_data_variable_xml_normalized(mbus_data_variable *data)
             }
             else
             {
+                free(buff);
+                return NULL;
             }
 
             len += snprintf(&buff[len], buff_size - len, "    </DataRecord>\n\n");
