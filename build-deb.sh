d479e17d3893f46819ef9c746746465ffaa1da09
Stefan Wahren <wahrenst@gmx.net>
2020-07-16 19:46:08 +0200

    Add build-deb.sh again

---
+++ build-deb.sh
@@ -0,0 +1,12 @@
+# ------------------------------------------------------------------------------
+#  Copyright (C) 2012, Robert Johansson <rob@raditex.nu>, Raditex Control AB
+#  All rights reserved.
+# 
+#  rSCADA 
+#  http://www.rSCADA.se
+#  info@raditex.nu
+# 
+# ------------------------------------------------------------------------------
+
+debuild -i -us -uc -b 
+#sudo pbuilder build $(NAME)_$(VERSION)-1.dsc
